42cfeab2ef8cda79e218db5e6ca87c0137a0e224
nanevenodd <2110631250021@student.unsika.ac.id>
2025-08-24 17:30:26 +0700

    awal

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,6 +1,6 @@
 export default function CollectionPage() {
   return (
-    <section className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 animate-fadeIn">
+    <section className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
       <div className="border border-yellow-100 rounded-lg p-4 shadow-md hover:shadow-lg transition">
         <h2 className="text-xl font-semibold mb-2">Produk 1</h2>
         <p className="text-sm text-gray-200">Deskripsi singkat produk, terinspirasi dari motif ulos Ragidup.</p>
